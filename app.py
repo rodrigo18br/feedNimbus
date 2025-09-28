2159f6fe30409d0db0bf1a14eef25513be48f50b
Rodrigo Oliveira <rodrigo18br@hotmail.com>
2025-09-28 17:13:53 -0300

    Change storing of configs from local files to browser cookies

--- app.py
+++ app.py
@@ -18,9 +18,7 @@ parser_headlines.add_argument('word')
 
 parser_feeds = reqparse.RequestParser()
 parser_feeds.add_argument('feeds', action='append')
-
-parser_stopwords = reqparse.RequestParser()
-parser_stopwords.add_argument('words', action='append')
+parser_feeds.add_argument('words', action='append')
 
 api = Api(app)
 
@@ -42,19 +40,12 @@ def headlines_js():
 @app.route('/wordcloud.js')
 def wordcloud_js():
     return current_app.send_static_file('js/wordcloud.js')
+@app.route('/cookies.js')
+def cookies_js():
+    return current_app.send_static_file('js/cookies.js')
 
 def sort_dict(dict):
     return sorted(dict.items(), key = lambda item: item[1], reverse = True)
-
-def get_feeds():
-    with open("feeds", "r") as f:
-        feeds=f.read()
-        return feeds.splitlines()
-
-def get_stopwords():
-    with open("stopwords", "r") as f:
-        stopwords=f.read()
-        return stopwords.splitlines()
 
 def get_articles(urls, word=None):
     import feedparser
@@ -67,38 +58,9 @@ def get_articles(urls, word=None):
         headlines = [headline for headline in headlines if word in headline['title']]
     return headlines
 
-def get_words(urls):
-    headlines = get_articles(urls)
-    stopwords = get_stopwords()
-    titles = [headline['title'] for headline in headlines]
 
-    tokens = []
-    for title in titles:
-        list_tokens = re.sub("[^a-zA-Zàâáôõóçóúíãéê'\s-]", '', title).split(' ')
-        for ind_token in range(len(list_tokens) - 1):
-          token = list_tokens[ind_token]
-          next_token = list_tokens[ind_token + 1]
-          if len(token) > 2 and token[0].isupper() == True and token.lower() not in stopwords:
-            if len(next_token) > 2 and next_token[0].isupper() == True and next_token.lower() not in stopwords:
-                tokens.append(token + " " + next_token)
-                continue
-            tokens.append(token)
-    count_tokens = {}
-    for token in tokens:
-        if token in count_tokens:
-            count_tokens[token] += 1
-            continue
-        count_tokens[token] = 1
-    count_tokens = {k: v for k, v in sort_dict(count_tokens)}
-    list_tokens = list(count_tokens.keys())[:70]
-    dict_tokens = {token: [headline for headline in headlines if token in headline['title']] for token in list_tokens}
-    
-    return dict_tokens
-
-def get_nouns(urls):
-    print(urls)
+def get_nouns(urls, stopwords):
     headlines = get_articles(urls)
-    stopwords = get_stopwords()
     titles = [headline['title'] for headline in headlines]
 
     tokens = []
@@ -123,9 +85,10 @@ def get_nouns(urls):
 # Routes
 class wordcloud(Resource):
     def post(self):
-        urls = get_feeds()
-        print(urls)
-        return {'words': get_nouns(urls)}, 200, { 'Access-Control-Allow-Origin' : '*', 'Access-Control-Allow-Headers': '*', 'Vary': 'Accept-Encoding, Origin' }
+        args = parser_feeds.parse_args()
+        feeds = args['feeds']
+        stopwords = args['words']
+        return {'words': get_nouns(feeds, stopwords)}, 200, { 'Access-Control-Allow-Origin' : '*', 'Access-Control-Allow-Headers': '*', 'Vary': 'Accept-Encoding, Origin' }
 
 class headlines(Resource):
     def post(self):
@@ -134,35 +97,8 @@ class headlines(Resource):
         word = args['word']
         return {'headlines': get_articles(urls, word)}, 200, { 'Access-Control-Allow-Origin' : '*' }
 
-class feeds(Resource):
-    def post(self):
-      args = parser_feeds.parse_args()
-      feeds = args['feeds']
-      with open("feeds", "w") as f:
-          f.write("\n".join(feeds))
-      return 200, { 'Access-Control-Allow-Origin' : '*' }
-      
-    def get(self):
-      urls = get_feeds()
-      return {'feeds': urls}, 200, { 'Access-Control-Allow-Origin' : '*' }
-
-class stopwords(Resource):
-    def post(self):
-      args = parser_stopwords.parse_args()
-      words = args['words']
-      with open("stopwords", "w") as f:
-          f.write("\n".join(words))
-      return 200, { 'Access-Control-Allow-Origin' : '*' }
-      
-    def get(self):
-      stopwords = get_stopwords()
-      return {'stopwords': stopwords}, 200, { 'Access-Control-Allow-Origin' : '*' }
-
-
 api.add_resource(wordcloud, '/wordcloud')
 api.add_resource(headlines, '/headlines')
-api.add_resource(feeds, '/feeds')
-api.add_resource(stopwords, '/stopwords')
 
 if __name__ == '__main__':
     nltk_download('punkt_tab')
